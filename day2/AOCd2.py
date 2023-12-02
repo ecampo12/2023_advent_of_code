b277b7cd12e98912b55fbed0e9920bfdee7332a2
Elvis Morales Campoverde <ecampo12@gmail.com>
2023-12-02 14:58:06 -0500

    completed part2 of day 2. 2 EZ

--- day2/AOCd2.py
+++ day2/AOCd2.py
@@ -20,14 +20,33 @@ def part1(input):
 
     return True
 
+def part2(input):
+    game_sets = input.split(":")[1].split(";")
+
+    fewest_red, fewest_green, fewest_blue = 0, 0, 0
+    for game_set in game_sets:
+        colors = re.findall(r"(\d+ (blue|red|green))", game_set)
+        for color in colors:
+            num = int(color[0].split(" ")[0])
+            if color[1] == "red" and int(num) > fewest_red:
+                fewest_red = num
+            elif color[1] == "blue" and int(num) > fewest_blue:
+                fewest_blue = num
+            elif color[1] == "green" and int(num) > fewest_green:
+                fewest_green = num
+
+    return fewest_red * fewest_green * fewest_blue
+
 def main():
     input = open("input.txt", "r").read().splitlines()
-    sum = 0
+    part1_sum, part2_sum = 0, 0
     for i in range(len(input)):
         if part1(input[i]):
-            sum += i + 1
+            part1_sum += i + 1
+        part2_sum += part2(input[i])
 
-    print(f"Part 1: {sum}")
+    print(f"Part 1: {part1_sum}")
+    print(f"Part 2: {part2_sum}")
 
 if __name__ == "__main__":
     main()
--- day2/test.py
+++ day2/test.py
@@ -20,10 +20,21 @@ class Test(unittest.TestCase):
         self.assertEqual(sum, 8)
         
 
-    # def test_part2(self):
-    #     input = []
-    #     expected  = []
-    #     asertTrue(False)
+    def test_part2(self):
+        input = [
+            "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green",
+            "Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue",
+            "Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red",
+            "Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red",
+            "Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green"
+        ]
+        expected  = [48, 12, 1560, 630, 36]
+        sum = 0
+        for i in range(len(input)):
+            power = part2(input[i])
+            self.assertEqual(power, expected[i])
+            sum += power
+        self.assertEqual(sum, 2286)
         
         
 if __name__ == "__main__":
