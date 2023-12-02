4b0849123c5ffad7b11f2470c1ad1f3cef24e52a
Elvis Morales Campoverde <ecampo12@gmail.com>
2023-12-01 23:22:24 -0500

    Script created: Day 2

---
+++ day2/test.py
@@ -0,0 +1,19 @@
+import unittest
+from AOCd2 import *
+
+class Test(unittest.TestCase):
+    def test_part1(self):
+        input = []
+        expected = []
+        asertTrue(False)
+
+    def test_part2(self):
+        input = []
+        expected  = []
+        asertTrue(False)
+        self.assertEqual(sum, 281)
+    
+        
+if __name__ == __main__:
+    unittest.main()
+    
