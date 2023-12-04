b30d729b219c012ebd6c539e2b595fee9fc908f7
Elvis Morales Campoverde <ecampo12@gmail.com>
2023-12-04 01:57:05 -0500

    Created day4 folder from python

---
+++ day4/AOC.py
@@ -0,0 +1,14 @@
+def part1(input):
+    return True
+
+def part2(input):
+    return True
+
+def main():
+    # input = open("input.txt", "r").read().splitlines()
+    part1, part2 = 0, 0
+    print(f"Part 1: {part1}")
+    print(f"Part 2: {part2}")
+
+if __name__ == "__main__":
+    main()
