d65663314ac6ff031ec77684446630ef59506e5a
Elvis Morales Campoverde <ecampo12@gmail.com>
2023-12-03 21:26:38 -0500

    Day 3 in progress

--- day3/test.py
+++ day3/test.py
@@ -3,14 +3,18 @@ from AOC import *
 
 class Test(unittest.TestCase):
     def test_part1(self):
-        input = open("test_input.txt", "r").read().splitlines()
-        expected = open("test_expected.txt", "r").read().splitlines()
-        self.assertTrue(False)
+        file = open("test_input.txt", "r")
+        input = file.read().splitlines()
+        file.close()
+        expected = 4361
+        sum = part1(input)
+        self.assertEqual(sum, expected)
+        
 
-    def test_part2(self):
-        input = open("test_input.txt", "r").read().splitlines()
-        expected  = open("test_expected.txt", "r").read().splitlines()
-        self.assertTrue(False)
+    # def test_part2(self):
+    #     input = open("test_input.txt", "r").read().splitlines()
+    #     expected  = open("test_expected.txt", "r").read().splitlines()
+    #     self.assertTrue(False)
         
 if __name__ == "__main__":
     unittest.main()
