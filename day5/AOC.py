145cc320247a590e255f0bd855c69f08b1209ea9
Elvis Morales Campoverde <ecampo12@gmail.com>
2023-12-05 16:52:06 -0500

    Passes tests, but too resource heavy. Refactoring.

--- day5/AOC.py
+++ day5/AOC.py
@@ -1,19 +1,71 @@
 import re
+import pprint
+
+class Almanac:
+    def __init__(self, seeds):
+        self.seeds = [int(seed) for seed in seeds]
+    
+    def add_map(self, map):
+        map = re.match(r"(\w+-\w+-\w+)", map.splitlines()[0])
+        print(map.group(1))
+
+def creat_map(input):
+    map = {}
+    input = input.splitlines()
+    name = input.pop(0)
+    
+    # print(input)
+    for desc in input:
+        values = [int(value) for value in re.findall(r"\d+", desc)]
+        
+        dest_start = values[0]
+        source_start = values[1]
+        range_len = values[2]
+        for i in range(range_len):
+            map[source_start + i] = dest_start + i
+            
+    return name, map
 
 def parse_input(input):
-    return input
+    sections = re.split(r'\n\s*\n', input)
+    seeds = [int(seed) for seed in re.findall(r"\d+", sections.pop(0))]
+    maps = {}
+    for sec in sections:
+        name, map = creat_map(sec)
+        maps[name] = map
+    return seeds, maps
 
 def part1(input):
-    return True
+    print("made it here")
+    locations = []
+    paths = []
+    seeds, maps = parse_input(input)
+    print("made it past parse_input")
+    
+    for seed in seeds:
+        # print(f"seed: {seed}")
+        path = [seed]
+        for key, map in maps.items():
+            # print(f"key: {key} seed: {seed}")
+            if seed not in map:
+                pass
+            else:
+                seed = map[seed]
+            path.append(seed)
+        paths.append(path)
+        locations.append(seed)
+    return locations, paths
 
 def part2(input):
     return True
 
 def main():
-    # input = open("input.txt", "r").read().splitlines()
-    part1_sum = 0
+    input = open("input.txt", "r").read()
+    locations, _ = part1(input)
+    # print(locations)
+    part1_answer = min(locations)
     part2_sum = 0
-    print(f"Part 1: {part1_sum}")
+    print(f"Part 1: {part1_answer}")
     print(f"Part 2: {part2_sum}")
 
 if __name__ == "__main__":
--- day5/test.py
+++ day5/test.py
@@ -2,19 +2,47 @@ import unittest
 from AOC import *
 
 class Test(unittest.TestCase):
+    # def test_parse_input(self):
+    #     file = open("test_input.txt", "r")
+    #     input = file.read()
+    #     file.close()
+    #     test_seeds = [79, 14, 55, 13]
+    #     test_soil = [81, 14, 57, 13]
+    #     seeds, map = parse_input(input)
+    #     seed_to_soil = map["seed-to-soil map:"]
+
+    #     for i in range(len(test_seeds)):
+    #         self.assertTrue(test_seeds[i] in seeds)
+    #         if test_seeds[i] == 14 or test_seeds[i] == 13:
+    #             with self.assertRaises(KeyError):
+    #                 seed_to_soil[test_seeds[i]]
+    #         else:
+    #             self.assertEqual(test_soil[i], seed_to_soil[test_seeds[i]])
+        
     def test_part1(self):
         file = open("test_input.txt", "r")
-        input = file.read().splitlines()
+        input = file.read()
         file.close()
-        expected = open("test_expected.txt", "r").read().splitlines()
-        self.assertTrue(False)
+        expected = [
+             [79, 81, 81, 81, 74, 78, 78, 82],
+             [14, 14, 53, 49, 42, 42, 43, 43],
+             [55, 57, 57, 53, 46, 82, 82, 86],
+             [13, 13, 52, 41, 34, 34, 35, 35]
+        ]
+        # parse_input(input)
+        locations, paths = part1(input)
+        for i in range(len(expected)):
+            self.assertListEqual(expected[i], paths[i])
+            self.assertEqual(expected[i][-1], locations[i])
+        # print(min(locations))
+        self.assertEqual(min(locations), 35)
 
-    def test_part2(self):
-        file = open("test_input.txt", "r")
-        input = file.read().splitlines()
-        file.close()
-        expected  = open("test_expected.txt", "r").read().splitlines()
-        self.assertTrue(False)
+    # def test_part2(self):
+    #     file = open("test_input.txt", "r")
+    #     input = file.read().splitlines()
+    #     file.close()
+    #     expected  = open("test_expected.txt", "r").read().splitlines()
+    #     self.assertTrue(False)
         
 if __name__ == "__main__":
     unittest.main()
