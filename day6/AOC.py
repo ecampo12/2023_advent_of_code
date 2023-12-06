0674ea0f2df862af032916146f20b394b1bb3c80
Elvis Morales Campoverde <ecampo12@gmail.com>
2023-12-06 12:15:24 -0500

    Part 1, Day 6 complete

--- day6/AOC.py
+++ day6/AOC.py
@@ -1,19 +1,34 @@
 import re
 
 def parse_input(input):
-    return input
+    records = input.splitlines()
+    times = [int(time) for time in re.findall(r"(\d+)", records[0].split(":")[1])]
+    distances = [int(distance) for distance in re.findall(r"(\d+)", records[1].split(":")[1])]
+    return times, distances
 
-def part1(input):
-    return True
+# Kinda brute force, but it works
+def part1(times, distances):
+    prod = 1
+    for time, distance in zip(times, distances):
+        possible_wins = [] # for debugging
+        for i in range(1, time):
+            if (time-i)*i > distance:
+                possible_wins.append((i, (time-i)*i))
+                
+        print(f"possible wins: {possible_wins}")
+        prod *= len(possible_wins)
+            
+    return prod
 
 def part2(input):
     return True
 
 def main():
-    # input = open("input.txt", "r").read().splitlines()
-    part1_sum = 0
+    input = open("input.txt", "r").read()
+    time, distance = parse_input(input)
+    part1_prod = part1(time, distance)
     part2_sum = 0
-    print(f"Part 1: {part1_sum}")
+    print(f"Part 1: {part1_prod}")
     print(f"Part 2: {part2_sum}")
 
 if __name__ == "__main__":
--- day6/test.py
+++ day6/test.py
@@ -4,17 +4,18 @@ from AOC import *
 class Test(unittest.TestCase):
     def test_part1(self):
         file = open("test_input.txt", "r")
-        input = file.read().splitlines()
+        input = file.read()
         file.close()
-        expected = open("test_expected.txt", "r").read().splitlines()
-        self.assertTrue(False)
+        times, distances = parse_input(input)
+        x = part1(times, distances)
+        self.assertEqual(x, 288)
 
-    def test_part2(self):
-        file = open("test_input.txt", "r")
-        input = file.read().splitlines()
-        file.close()
-        expected  = open("test_expected.txt", "r").read().splitlines()
-        self.assertTrue(False)
+    # def test_part2(self):
+    #     file = open("test_input.txt", "r")
+    #     input = file.read()
+    #     file.close()
+    #     expected  = open("test_expected.txt", "r").read().splitlines()
+    #     self.assertTrue(False)
         
 if __name__ == "__main__":
     unittest.main()
