8f22e53787c26cd99042e470896bcb04c2101374
Elvis Morales Campoverde <ecampo12@gmail.com>
2023-12-08 13:22:47 -0500

    Python: Day8 part 1 complete

--- Python/day8/AOC.py
+++ Python/day8/AOC.py
@@ -1,19 +1,34 @@
 import re
 
 def parse_input(input):
-    return input
+    instructions = [0 if c == 'L' else 1 for c in input[0]]
+    routes = {}
+    for i in range(2, len(input)):
+        x = re.findall(r"\w+", input[i])
+        routes[x[0]] = (x[1], x[2])
+    
+    return instructions, routes
 
-def part1(input):
-    return True
+def part1(instructions, routes):
+    curr_inst = instructions[0]
+    curr_loc = 'AAA'
+    count = 1
+    while curr_loc != 'ZZZ':
+        curr_loc = routes[curr_loc][curr_inst]
+        curr_inst = instructions[count%len(instructions)]
+        count += 1
+        print(f"{curr_loc} {curr_inst}")
+    return count - 1
 
 def part2(input):
     return True
 
 def main():
-    # input = open("input.txt", "r").read().splitlines()
-    part1_sum = 0
+    input = open("input.txt", "r").read().splitlines()
+    instructions, routes = parse_input(input)
+    part1_count = part1(instructions, routes)
     part2_sum = 0
-    print(f"Part 1: {part1_sum}")
+    print(f"Part 1: {part1_count}")
     print(f"Part 2: {part2_sum}")
 
 if __name__ == "__main__":
--- Python/day8/test.py
+++ Python/day8/test.py
@@ -6,15 +6,24 @@ class Test(unittest.TestCase):
         file = open("test_input.txt", "r")
         input = file.read().splitlines()
         file.close()
-        expected = open("test_expected.txt", "r").read().splitlines()
-        self.assertTrue(False)
-
-    def test_part2(self):
-        file = open("test_input.txt", "r")
+        instructions, routes = parse_input(input)
+        total = part1(instructions, routes)
+        self.assertEqual(total, 2)
+        
+    def test_part1_example2(self):
+        file = open("test_input2.txt", "r")
         input = file.read().splitlines()
         file.close()
-        expected  = open("test_expected.txt", "r").read().splitlines()
-        self.assertTrue(False)
+        instructions, routes = parse_input(input)
+        total = part1(instructions, routes)
+        self.assertEqual(total, 6)
+
+    # def test_part2(self):
+    #     file = open("test_input.txt", "r")
+    #     input = file.read().splitlines()
+    #     file.close()
+    #     expected  = open("test_expected.txt", "r").read().splitlines()
+    #     self.assertTrue(False)
         
 if __name__ == "__main__":
     unittest.main()
