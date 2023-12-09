d275ea74f3b3de132d71c67d4511af4324b6518f
Elvis Morales Campoverde <ecampo12@gmail.com>
2023-12-09 10:47:34 -0500

    Python: Day 9, part 2 comlete. 2 EZ

--- Python/day9/AOC.py
+++ Python/day9/AOC.py
@@ -9,6 +9,12 @@ def getNextNumber(input):
     diff = [input[i+1] - input[i] for i in range(len(input)-1)]
     return input[-1] + getNextNumber(diff)
 
+def getPrevNumber(input):
+    if all(x == 0 for x in input):
+        return 0
+    diff = [input[i+1] - input[i] for i in range(len(input)-1)]
+    return input[0] - getPrevNumber(diff)
+
 def part1(input):
     next_nums = []
     for line in input:
@@ -17,13 +23,17 @@ def part1(input):
     return sum(next_nums)
 
 def part2(input):
-    return True
+    prev_nums = []
+    for line in input:
+        x = getPrevNumber(line)
+        prev_nums.append(x)
+    return sum(prev_nums)
 
 def main():
     input = open("input.txt", "r").read().splitlines()
     nums = parse_input(input)
     part1_sum = part1(nums)
-    part2_sum = 0
+    part2_sum = part2(nums)
     print(f"Part 1: {part1_sum}")
     print(f"Part 2: {part2_sum}")
 
--- Python/day9/test.py
+++ Python/day9/test.py
@@ -10,11 +10,13 @@ class Test(unittest.TestCase):
         next_nums = part1(input)        
         self.assertEqual(next_nums, 114)
 
-    # def test_part2(self):
-    #     file = open("test_input.txt", "r")
-    #     input = file.read().splitlines()
-    #     file.close()
-    #     self.assertTrue(False)
+    def test_part2(self):
+        file = open("test_input.txt", "r")
+        input = file.read().splitlines()
+        file.close()
+        input = parse_input(input)
+        next_nums = part2(input)        
+        self.assertEqual(next_nums, 2)
         
 if __name__ == "__main__":
     unittest.main()
