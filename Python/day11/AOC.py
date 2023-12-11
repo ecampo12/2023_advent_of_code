d978c12d64bc4a6fe690650ea9a355715970ab2f
Elvis Morales Campoverde <ecampo12@gmail.com>
2023-12-11 11:32:30 -0500

    Python: Day 11, Part 2 complete. 2 EZ

--- Python/day11/AOC.py
+++ Python/day11/AOC.py
@@ -21,12 +21,12 @@ def parse_input(input):
     return galaxy, empty_row, empty_col
 
 # Middle shcool math, baby!
-def distance(start, end, empty_rows, empty_cols):
+def distance(start, end, empty_rows, empty_cols, expand=2):
     manhattan_dist = abs(start[0] - end[0]) + abs(start[1] - end[1])
 
     # Additional distance for crossing empty rows and columns
-    row_crossings = len([row for row in empty_rows if min(start[0], end[0]) < row < max(start[0], end[0])])
-    col_crossings = len([col for col in empty_cols if min(start[1], end[1]) < col < max(start[1], end[1])])
+    row_crossings = (expand - 1)*len([row for row in empty_rows if min(start[0], end[0]) < row < max(start[0], end[0])])
+    col_crossings = (expand - 1)*len([col for col in empty_cols if min(start[1], end[1]) < col < max(start[1], end[1])])
 
     return manhattan_dist + row_crossings + col_crossings
 
@@ -48,14 +48,22 @@ def part1(galaxy, empty_rows, empty_cols):
     
     return sum(dists)
 
-def part2(input):
-    return True
+def part2(galaxy, empty_rows, empty_cols, expand=1):
+    dists = []
+    combos = find_all_galaxy_pairs(galaxy)    
+    for combo in combos:
+        start = combo[0][1]
+        end = combo[1][1]
+        dist = distance(start, end, empty_rows, empty_cols, expand)
+        dists.append(dist)
+    
+    return sum(dists)
 
 def main():
     input = open("input.txt", "r").read().splitlines()
     galaxies, rows, cols = parse_input(input)
     part1_sum = part1(galaxies, rows, cols)
-    part2_sum = 0
+    part2_sum = part2(galaxies, rows, cols, 1000000)
     print(f"Part 1: {part1_sum}")
     print(f"Part 2: {part2_sum}")
 
--- Python/day11/test.py
+++ Python/day11/test.py
@@ -10,12 +10,17 @@ class Test(unittest.TestCase):
         dist_sum = part1(galaxies, rows, cols)
         self.assertEqual(dist_sum, 374)
 
-    # def test_part2(self):
-    #     file = open("test_input.txt", "r")
-    #     input = file.read().splitlines()
-    #     file.close()
-    #     expected  = open("test_expected.txt", "r").read().splitlines()
-    #     self.assertTrue(False)
+    def test_part2(self):
+        file = open("test_input.txt", "r")
+        input = file.read().splitlines()
+        file.close()
+        
+        galaxies, rows, cols = parse_input(input)
+        dist_sum10 = part2(galaxies, rows, cols, 10)
+        self.assertEqual(dist_sum10, 1030)
+        
+        dist_sum100 = part2(galaxies, rows, cols, 100)
+        self.assertEqual(dist_sum100, 8410)
         
 if __name__ == "__main__":
     unittest.main()
