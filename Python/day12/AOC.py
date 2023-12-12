7db6e9b4bc687e27fdc26324d3afdd0b5eb087b7
Elvis Morales Campoverde <ecampo12@gmail.com>
2023-12-12 09:11:56 -0500

    Created by script: Day12

---
+++ Python/day12/AOC.py
@@ -0,0 +1,20 @@
+import re
+
+def parse_input(input):
+    return input
+
+def part1(input):
+    return True
+
+def part2(input):
+    return True
+
+def main():
+    # input = open("input.txt", "r").read().splitlines()
+    part1_sum = 0
+    part2_sum = 0
+    print(f"Part 1: {part1_sum}")
+    print(f"Part 2: {part2_sum}")
+
+if __name__ == "__main__":
+    main()
