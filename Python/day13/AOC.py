dd71e23a5075942ce8443ea16ff7e85b11e4eb02
Elvis Morales Campoverde <ecampo12@gmail.com>
2023-12-13 14:31:16 -0500

    Python: Day 13, part 1 complete

--- Python/day13/AOC.py
+++ Python/day13/AOC.py
@@ -1,17 +1,49 @@
 import re
 
 def parse_input(input):
-    return input
+    lines = []
+    for x in re.split("\n\n", input):
+        lines.append([line for line in x.split("\n") if line != ""])
+    return lines
 
+def reverse_list(input):
+    rev = []
+    for i in range(len(input), 0, -1):
+        rev.append(input[i - 1])
+    return rev
+
+def get_horizontal(input):
+    for row in range(1, len(input)):
+        top = reverse_list(input[:row])
+        bottom = input[row:]
+        
+        # makes sure both halfs are the same length
+        top = top[:len(bottom)]
+        bottom = bottom[:len(top)]
+        
+        if top == bottom:
+            return (row, row + 1) # tuple for debugging
+    return (0, 0)
+    
 def part1(input):
-    return True
+    total = 0
+    for pattern in input:
+        hor = get_horizontal(pattern)
+        print(hor)
+        total += hor[0] * 100
+        col = get_horizontal(list(zip(*pattern))) # zip trick to transpose matrix
+        print(col)
+        total += col[0]
+        print()
+    return total
 
 def part2(input):
     return True
 
 def main():
-    # input = open("input.txt", "r").read().splitlines()
-    part1_sum = 0
+    input = open("input.txt", "r").read()
+    x = parse_input(input)
+    part1_sum = part1(x)
     part2_sum = 0
     print(f"Part 1: {part1_sum}")
     print(f"Part 2: {part2_sum}")
--- Python/day13/test.py
+++ Python/day13/test.py
@@ -4,17 +4,18 @@ from AOC import *
 class Test(unittest.TestCase):
     def test_part1(self):
         file = open("test_input.txt", "r")
-        input = file.read().splitlines()
+        input = file.read()
         file.close()
-        expected = open("test_expected.txt", "r").read().splitlines()
-        self.assertTrue(False)
+        x = parse_input(input)
+        y = part1(x)
+        self.assertEqual(y, 405)
 
-    def test_part2(self):
-        file = open("test_input.txt", "r")
-        input = file.read().splitlines()
-        file.close()
-        expected  = open("test_expected.txt", "r").read().splitlines()
-        self.assertTrue(False)
+    # def test_part2(self):
+    #     file = open("test_input.txt", "r")
+    #     input = file.read().splitlines()
+    #     file.close()
+    #     expected  = open("test_expected.txt", "r").read().splitlines()
+    #     self.assertTrue(False)
         
 if __name__ == "__main__":
     unittest.main()
