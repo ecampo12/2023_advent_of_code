a3aeef52252654b2eb9a2d69a6b5886c888e8d6a
Elvis Morales Campoverde <ecampo12@gmail.com>
2023-12-18 15:55:07 -0500

    Uncommented day 18 tests

--- Python/day18/test.py
+++ Python/day18/test.py
@@ -2,13 +2,13 @@ import unittest
 from AOC import *
 
 class Test(unittest.TestCase):
-    # def test_part1(self):
-    #     file = open("test_input.txt", "r")
-    #     input = file.read().splitlines()
-    #     file.close()
-    #     inst = parse_input(input)
-    #     x = part1(inst)
-    #     self.assertEqual(x, 62)
+    def test_part1(self):
+        file = open("test_input.txt", "r")
+        input = file.read().splitlines()
+        file.close()
+        inst = parse_input(input)
+        x = part1(inst)
+        self.assertEqual(x, 62)
 
     def test_part2(self):
         file = open("test_input.txt", "r")
