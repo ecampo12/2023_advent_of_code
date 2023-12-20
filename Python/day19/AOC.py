ac13b6d4f9e4afbb73f73513346c0df571a0ef77
Elvis Morales Campoverde <ecampo12@gmail.com>
2023-12-19 21:51:50 -0500

    Python: refactored day 19 part1, part2 complete

--- Python/day19/AOC.py
+++ Python/day19/AOC.py
@@ -1,12 +1,38 @@
 import re
 class Parts:
-    cat_map = {}
     def __init__(self, x, m, a, s):
+        self.cat_map = {}
         self.cat_map = {'x': int(x), 'm': int(m), 'a': int(a), 's': int(s)}
-    
-    def __str__(self) -> str:
-        return f"Part: {self.cat_map}"
+
+class WorkFlow:
+    class Rule: # Doesn't need to be an inner class, but whatever
+        def __init__(self, rule):
+            eq, self.next = rule.split(":")
+            if "<" in rule:
+                self.op = "<"
+                self.key, self.val = eq.split("<")
+                self.val = int(self.val)
+            elif ">" in rule:
+                self.op = ">"
+                self.key, self.val = eq.split(">")
+                self.val = int(self.val)
         
+    def __init__(self, label, rules):
+        self.rules = []
+        self.label = label
+        rules = rules.split(",")
+        self.default = rules[-1]
+        for rule in rules[:-1]:
+            self.rules.append(self.Rule(rule))
+    
+    def eval_part(self, part: Parts):
+        for rule in self.rules:
+            # eval is bad, but it's the easiest way to do this
+            if eval(f"part.cat_map[rule.key] {rule.op} rule.val"):
+                return rule.next
+        else:
+            return self.default
+
 def parse_input(input: str): 
     work_flow_map = {}
     parsed_parts = []
@@ -14,8 +40,7 @@ def parse_input(input: str):
     flows = workflows.split("\n")
     for flow in flows:
         label, rules = re.findall(r"[^{|}]+", flow)
-        rules = rules.split(",")
-        work_flow_map[label] = rules
+        work_flow_map[label] = WorkFlow(label, rules)
     
     parts = parts.split("\n")
     for part in parts:
@@ -30,73 +55,62 @@ def part1(work_flow_map, parts):
     for p in parts:
         key = "in"
         work_flow = work_flow_map["in"]
-        sorted = False
-        while not sorted:
-            print(work_flow)
-            for rule in work_flow:
-                if '<' in rule:
-                    category, rest = rule.split("<")
-                    val, dest = rest.split(":")
-                    if p.cat_map[category] < int(val):
-                        if dest == 'A':
-                            print(f"Accepted: {p}")
-                            accepted_parts.append(p)
-                            sorted = True
-                            break
-                        elif dest == 'R':
-                            print(f"Rejected: {p}")
-                            sorted = True
-                            break
-                        else:
-                            work_flow = work_flow_map[dest]
-                            print(f"From: {key} New work flow: {dest}")
-                            key = dest
-                            break
-                elif '>' in rule:
-                    category, rest = rule.split(">")
-                    val, dest = rest.split(":")
-                    if p.cat_map[category] > int(val):
-                        if dest == 'A':
-                            print(f"Accepted: {p}")
-                            accepted_parts.append(p)
-                            sorted = True
-                            break
-                        elif dest == 'R':
-                            print(f"Rejected: {p}")
-                            sorted = True
-                            break
-                        else:
-                            work_flow = work_flow_map[dest]
-                            break
-                elif rule == 'R':
-                    print(f"Rejected: {p}")
-                    sorted = True
-                    break
-                elif rule == 'A':
-                    print(f"Accepted: {p}")
-                    accepted_parts.append(p)
-                    sorted = True
-                    break
-                else:
-                    print(f"New work flow: {rule}")
-                    work_flow = work_flow_map[rule]
-                    break
-                    
-    print(len(accepted_parts))
+        while True:
+            work_flow = work_flow_map[key]
+            key = work_flow.eval_part(p)
+
+            if key == "A":
+                accepted_parts.append(p)
+                break
+            elif key == "R":
+                break
     total = 0
     for p in accepted_parts:
         total += sum(list(p.cat_map.values()))
-    print(total)
     return total
 
-def part2(input):
-    return True
+def find_ranges(work_flow_map, ranges, label="in"):
+    if label == "R":
+        return 0
+    if label == "A":
+        prod = 1
+        for start, end in ranges.values():
+            prod *= (end - start) + 1
+        return prod
+        
+    workflow = work_flow_map[label]
+    total = 0
+    for rule in workflow.rules:
+        start, end = ranges[rule.key]
+        if "<" == rule.op:
+            new_ranges = (start, min(rule.val - 1, end))
+            rest = (max(start, rule.val), end)
+        else:
+            new_ranges = (max(start, rule.val + 1), end)
+            rest = (start, min(rule.val, end))
+            
+        if new_ranges[0] <= new_ranges[1]:
+            copy = dict(ranges)
+            copy[rule.key] = new_ranges
+            total += find_ranges(work_flow_map, copy, rule.next)
+        if rest[0] <= rest[1]:
+            ranges = dict(ranges)
+            ranges[rule.key] = rest
+        else:
+            break
+    else:
+        total += find_ranges(work_flow_map, ranges, workflow.default)
+    return total
+
+def part2(work_flow_map):
+    m = {"x": (1, 4000), "m": (1, 4000), "a": (1, 4000), "s": (1, 4000)}
+    return find_ranges(work_flow_map, m)
 
 def main():
     input = open("input.txt", "r").read()
     w, p = parse_input(input)
     part1_sum = part1(w, p)
-    part2_sum = 0
+    part2_sum = part2(w)
     print(f"Part 1: {part1_sum}")
     print(f"Part 2: {part2_sum}")
 
--- Python/day19/test.py
+++ Python/day19/test.py
@@ -10,11 +10,13 @@ class Test(unittest.TestCase):
         sum = part1(w, p)
         self.assertEqual(sum, 19114)
         
-    # def test_part2(self):
-    #     file = open("test_input.txt", "r")
-    #     input = file.read().splitlines()
-    #     file.close()
-    #     self.assertTrue(False)
+    def test_part2(self):
+        file = open("test_input.txt", "r")
+        input = file.read()
+        file.close()
+        w, _ = parse_input(input)
+        total = part2(w)
+        self.assertEqual(total, 167409079868000)
         
 if __name__ == "__main__":
     unittest.main()
